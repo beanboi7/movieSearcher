2ee44779f642a5f38845335e9a198048417a8ac7
Sriram J <61819503+beanboi7@users.noreply.github.com>
2020-10-01 15:14:55 +0530

    deleting unwanted stuff

--- src/Searchbar.js
+++
@@ -1,5 +0,0 @@
-
-// const read_access_token = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmYWIyNzI0YTJiYTMxOGRjN2QwOGQwMzkwOGEwZjVlOSIsInN1YiI6IjVmMTliMjI2Y2EwZTE3MDAzNDkxODc2MiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.JX0VqEpGwwI0JKskzJS2pEIQSaqZRk8Ec3jEj1tOpEE';
-// const example_req = 'https://api.themoviedb.org/3/movie/550?api_key=fab2724a2ba318dc7d08d03908a0f5e9';
-
-
